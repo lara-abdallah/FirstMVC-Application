7fa3b70ddc3bac18c32f1889dcddb8fbef29863f
laraabdallah <laraabdallah478@gmail.com>
2020-03-28 16:09:05 +0200

    Add project files.

---
+++ FirstMVC-Application/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using FirstMVC_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC_Application.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetStudentName() 
+        {
+            Student student = new Student();
+            student.StudentName = "Lara";
+            return View(student);
+        }
+    }
+}
---
+++ FirstMVC-Application/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMVC_Application.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+    }
+}
